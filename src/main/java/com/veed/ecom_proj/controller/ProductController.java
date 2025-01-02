9947b72a3dd7a61af92d6f18824041d4144b43a7
David Onungwe <onungwed@gmail.com>
2025-01-02 17:18:47 -0500

    update and delete logic implemented

--- src/main/java/com/veed/ecom_proj/controller/ProductController.java
+++ src/main/java/com/veed/ecom_proj/controller/ProductController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -58,5 +59,31 @@ public class ProductController {
         byte[] imageFile = product.getImageData();
 
         return ResponseEntity.ok().contentType(MediaType.valueOf(product.getImageType())).body(imageFile);
+    }
+
+    public ResponseEntity<String> updateProduct(@PathVariable int id, @RequestPart Product product,
+                                                @RequestPart MultipartFile imageFile){
+        Product product1 = null;
+        try {
+            product1 = service.updateProduct(id, product, imageFile);
+        } catch (IOException e) {
+            return new ResponseEntity<>("Failed to update", HttpStatus.BAD_REQUEST);
+        }
+        if(product1 != null)
+            return new ResponseEntity<>("Updated", HttpStatus.OK);
+        else
+            return new ResponseEntity<>("Failed to update", HttpStatus.BAD_REQUEST);
+    }
+
+    @DeleteMapping("/product/{id}")
+    public ResponseEntity<String> deleteProduct(@PathVariable int id){
+
+        Product product = service.getProductById(id);
+        if(product != null) {
+            service.deleteProduct(id);
+            return new ResponseEntity<>("Deleted", HttpStatus.OK);
+        }
+        else
+            return new ResponseEntity<>("Product5 not found", HttpStatus.NOT_FOUND);
     }
 }
--- src/main/java/com/veed/ecom_proj/service/ProductService.java
+++ src/main/java/com/veed/ecom_proj/service/ProductService.java
@@ -30,4 +30,17 @@ public class ProductService {
 
         return repository.save(product);
     }
+
+    public Product updateProduct(int id, Product product, MultipartFile imageFile) throws IOException {
+
+        product.setImageDate(imageFile.getBytes());
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageType(imageFile.getContentType());
+        return repository.save(product);
+    }
+
+    public void deleteProduct(int id) {
+
+        repository.deleteById(id);
+    }
 }
